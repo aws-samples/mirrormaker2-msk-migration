f8b2deedfb5a41bd2f608e4b2270b8ec784ca9a6
Igors Kugaudo <Igors.Kugaudo@awtg.co.uk>
2022-12-26 15:44:10 +0200

    [-] Dockerization

---
+++ CustomMM2ReplicationPolicy/src/main/java/com/amazonaws/kafka/samples/App.java
@@ -0,0 +1,9 @@
+package com.amazonaws.kafka.samples;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("It works");
+        // do some stuff like ...
+        // CustomMM2ReplicationPolicy policy = new CustomMM2ReplicationPolicy();
+    }
+}
